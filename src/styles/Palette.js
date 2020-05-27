1d99ce88f789d715647be643250b56261e05d869
BV <ufo62s@gmail.com>
2020-05-28 01:39:37 +0200

    media query for palette.js

--- src/styles/Palette.js
+++ src/styles/Palette.js
@@ -9,5 +9,9 @@ export default {
     height: "90vh",
     display: "grid",
     gridTemplateColumns: "repeat(auto-fill, minmax(350px, 1fr))"
+    
+    "@media (max-width: 450px)": {
+      minHeight: "100vh",
+    },
   }
 };
